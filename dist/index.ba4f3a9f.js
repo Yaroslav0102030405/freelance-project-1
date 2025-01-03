1c24405d825a95c248602cbaadfdd5d113a0ee07
Yaroslav0102030405 <you@example.com>
2025-01-03 18:13:42 +0200

    modal-2

--- dist/index.ba4f3a9f.js
+++ dist/index.ba4f3a9f.js
@@ -1,33 +1,17 @@
 // скрипт делегування щоб в контенере при натискі на будь яку кнопку відкривилося модалька
 const refsContainerButton = {
     containerEl: document.querySelector('.js-modal'),
-    //   closeModalBtn: document.querySelector('.js-modal'),
     backdrop: document.querySelector('.js-backdrop')
 };
 refsContainerButton.containerEl.addEventListener('click', onClickButton);
-// refsContainerButton.closeModalBtn.addEventListener('click', onCloseModal);
-// refsContainerButton.backdrop.addEventListener('click', onBackdropModal);
 function onClickButton(e) {
     if (!e.target.classList.contains('btn')) return;
     console.log(e.target);
-    //   window.addEventListener('keydown', onEscKeyPress);
-    document.body.classList.add('show-modal');
     onCloseModal();
-//   refsContainerButton.modalTitle.textContent = e.target.textContent;
 }
 function onCloseModal() {
-    //   window.removeEventListener('keydown', onEscKeyPress);
     document.body.classList.remove('show-modal');
     refsContainerButton.backdrop.classList.add('is-hidden');
-} // function onBackdropModal(event) {
- //   if (event.currentTarget === event.target) {
- //     onCloseModal();
- //   }
- // }
- // function onEscKeyPress(event) {
- //   if (event.code === 'Escape') {
- //     onCloseModal();
- //   }
- // }
+}
 
 //# sourceMappingURL=index.ba4f3a9f.js.map
