4ae9c6e8587db4ac325e12132a51ab3c27a7d790
Yaroslav0102030405 <you@example.com>
2025-01-05 20:53:32 +0200

    modules files

--- src/js/modal-2.js
+++ src/js/modal-2.js
@@ -1,28 +1,28 @@
 const refsContainerButton2 = {
-  containerEl: document.querySelector('body'),
-  openModal: document.querySelector('.js-button'),
-  closeModalBtn: document.querySelector('.js-close-modal'),
-  backdrop2: document.querySelector('.js-backdrop-2'),
+  containerRef: document.querySelector('body'),
+  openModalRef: document.querySelector('.js-button'),
+  closeModalBtnRef: document.querySelector('.js-close-modal'),
+  backdrop2Ref: document.querySelector('.js-backdrop-2'),
   //   modalTitle: document.querySelector('.js-modal-title'),
 };
 
-refsContainerButton2.containerEl.addEventListener('click', onClickButton2);
-refsContainerButton2.closeModalBtn.addEventListener('click', onCloseModal2);
-refsContainerButton2.backdrop2.addEventListener('click', onBackdropModal2);
+refsContainerButton2.containerRef.addEventListener('click', onClickButton2);
+refsContainerButton2.closeModalBtnRef.addEventListener('click', onCloseModal2);
+refsContainerButton2.backdrop2Ref.addEventListener('click', onBackdropModal2);
 
 function onClickButton2(e) {
   console.log(e);
   if (!e.target.classList.contains('js-button')) {
     return;
   }
-  refsContainerButton2.backdrop2.classList.remove('is-hidden-2');
+  refsContainerButton2.backdrop2Ref.classList.remove('is-hidden-2');
   window.addEventListener('keydown', onEscKeyPress2);
   document.body.classList.add('show-modal');
   //   refsContainerButton.modalTitle.textContent = e.target.textContent;
 }
 
 function onCloseModal2() {
-  refsContainerButton2.backdrop2.classList.add('is-hidden-2');
+  refsContainerButton2.backdrop2Ref.classList.add('is-hidden-2');
   window.removeEventListener('keydown', onEscKeyPress2);
   document.body.classList.remove('show-modal');
 }
