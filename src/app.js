c502ffcd7757407740016e45f16a6bbf1f59cb8f
Yaroslav0102030405 <you@example.com>
2025-01-06 23:17:33 +0200

    ew

--- src/app.js
+++ src/app.js
@@ -4,3 +4,11 @@
 new Splide('#slider1').mount();
 new Splide('#slider2').mount();
 new Splide('#slider3').mount();
+
+import './js/lang';
+import './js/modal';
+import './js/modal-2';
+import './js/scrollPage';
+import './js/smoothscroll';
+import './js/telegram-1';
+import './js/telegram-2';
