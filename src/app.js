93a8bd8ba830c9e7df67cc99725f296332092152
Yaroslav0102030405 <lukovetskyi@gmail.com>
2025-11-13 15:21:06 +0200

    v6

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ new Splide('#slider4').mount();
 new Splide('#slider5').mount();
 new Splide('#slider6').mount();
 new Splide('#slider7').mount();
-new Splide('#slider8').mount();
+// new Splide('#slider8').mount();
 new Splide('#slider9').mount();
-new Splide('#slider10').mount();
+// new Splide('#slider10').mount();
 new Splide('#slider11').mount();
-new Splide('#slider12').mount();
+// new Splide('#slider12').mount();
 
 // Щоб анімація була послудовною AOS
 const stack = [];
